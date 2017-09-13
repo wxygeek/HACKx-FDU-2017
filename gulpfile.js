9582b8b8a4017ef05fb858f4119ea6577d4fec11
Xiaoyu Wang <xiaoyu.wang@autodesk.com>
2017-09-13 23:59:41 +0800

    Refine page style & content

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,8 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
+    // Reloads the browser whenever HTML or JS CSS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('css/**/*.css', browserSync.reload);
 });
--- js/creative.js
+++ js/creative.js
@@ -45,43 +45,4 @@
         distance: '0px'
     }, 300);
 
-    var timeIntervalTime = 150;
-    var displayTime = 700;
-
-    // HACK x with different words
-    var words = ['FDU', 'Business', 'Finance', 'Art', 'Health', 'China', 'Ideas', 'Geek', 'Design'];
-    var currentWordIndex = 0;
-    var currentWordSubstrLen = 4;
-    var isFinished = true;
-    var timeInterval = timeIntervalTime;
-
-    function flushContent () {
-        if(isFinished) {
-            timeInterval = timeIntervalTime;
-            currentWordSubstrLen--;
-            if(currentWordSubstrLen < 0) {
-                //展示新词
-                currentWordIndex++;
-                currentWordIndex = currentWordIndex < words.length ? currentWordIndex : 0;
-                currentWordSubstrLen = 1;
-                isFinished = false;
-            }
-        } else {
-            currentWordSubstrLen++;
-            timeInterval = timeIntervalTime;
-            if(currentWordSubstrLen === words[currentWordIndex].length) {
-                isFinished = true;
-                timeInterval = displayTime;
-            }
-        }
-        var text = words[currentWordIndex].substr(0, currentWordSubstrLen);
-        if(currentWordSubstrLen === 0) {
-            // text = '<br>';
-            text = (window.screen.width < 768 ? '<br>' : '');
-        }
-        $('#hackx_header_content').html(text);
-        setTimeout(flushContent, timeInterval);
-    }
-    setTimeout(flushContent, displayTime);
-
 })(jQuery); // End of use strict
